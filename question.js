6be1c7b0e55e1d5d87f8abbf1f3aeb1c59929039
Kallyan Singha <singhakallyan@gmail.com>
2022-01-07 11:30:21 +0530

    init

---
+++ question.js
@@ -0,0 +1,82 @@
+let question = [
+    {
+        qs :" No of colors are there in Indian National Flag ?",
+        o1 : "three",
+        o2 : "Four",
+        o3 : "five",
+        o4 : "none",
+        ans: "Four"
+    },
+    {
+        qs :" what is the name of the Current Prime Minister of India?",
+        o1 : "Mamata Banarjee",
+        o2 : "Narendra Modi",
+        o3 : "Jawarlal Neheru",
+        o4 : "Indira Gandhi",
+        ans: "Narendra Modi"
+    },
+    {
+        qs :" Whats is Crypto currency?",
+        o1 : "Physical Currency",
+        o2 : "Currency of Canada",
+        o3 : "Currency of Cuba",
+        o4 : "Virtual Currency",
+        ans: "Virtual Currency"
+    },
+    {
+        qs :" Which city is known as the IT hub of India ?",
+        o1 : "Kolkata",
+        o2 : "Hydrabad",
+        o3 : "Mumbai",
+        o4 : "Bangalore",
+        ans: "Hydrabad"
+    },
+    {
+        qs :"Where is the Rastrapati Bhaban is located ?",
+        o1 : "Mumbai",
+        o2 : "Hydrabad",
+        o3 : "Kolkata",
+        o4 : "Delhi",
+        ans: "Delhi"
+    },
+    {
+        qs :" who is known as the missile man of India ?",
+        o1 : "APJ Abdul Kalam",
+        o2 : "Dr. mohummad",
+        o3 : "Jawarlal Neheru",
+        o4 : "Narendra Modi",
+        ans: "APJ Abdul Kalam"
+    },
+    {
+        qs :" No of colors are there in Indian National Flag ?",
+        o1 : "three",
+        o2 : "Four",
+        o3 : "five",
+        o4 : "none",
+        ans: "Four"
+    },
+    {
+        qs :" What is the Position of Indian Air Force in world Air Power Index?",
+        o1 : "4",
+        o2 : "1",
+        o3 : "6",
+        o4 : "2",
+        ans: "4"
+    },
+    {
+        qs :" No of colors are there in Indian National Flag ?",
+        o1 : "three",
+        o2 : "Four",
+        o3 : "five",
+        o4 : "none",
+        ans: "Four"
+    },
+    {
+        qs :" No of colors are there in Indian National Flag ?",
+        o1 : "three",
+        o2 : "Four",
+        o3 : "five",
+        o4 : "none",
+        ans: "Four"
+    },
+]
